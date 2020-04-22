2b4562eaff8d10e942e4ff9c20f21426ba40fac9
Krishtapovich <MKRISHTAPOVICH@gmail.com>
2020-04-22 17:36:06 +0300

    semestr2

---
+++ semestr2/СPlusPlus/LR7/HashTable.h
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------------
+#include <vcl.h>
+#ifndef HashTableH
+#define HashTableH
+class HashNode
+{
+public:
+	int key;
+    int nodes;
+	AnsiString info;
+	HashNode *pNext;
+	HashNode(int, AnsiString);
+};
+
+class HashTable
+{
+private:
+	static const int size = 30;
+	HashNode **elements;
+	int Hash(int);
+	void Swap(HashNode*, HashNode*);
+public:
+	void Create();
+	void Add(int, AnsiString);
+	void Delete(TMemo*, int);
+	void Show(TMemo*);
+	void Search(TMemo*, int);
+	void Decrease(TMemo*);
+};
+//---------------------------------------------------------------------------
+#endif
